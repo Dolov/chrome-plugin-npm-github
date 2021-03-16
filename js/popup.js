403472bb87406b58de3eebebd092cf57a814b6c1
shisongyan <songyan_shi@163.com>
2021-03-16 22:51:48 +0800

    v2

--- js/popup.js
+++ js/popup.js
@@ -8,12 +8,23 @@ const ReviewApp = {
         searchText: '查询',
         searchValue: '',
         searchResponse: null,
+        currentChecked: null,
       }
+  },
+  mounted() {
+    this.$refs.inputRef.focus()
   },
   methods: {
     onChange(event) {
       this.searchValue = event.target.value
-    }, 
+    },
+    onCheck(checkedValue) {
+      this.currentChecked = checkedValue
+    },
+    getGithub1s(url) {
+      if (!url) return null
+      return url.replace('github', 'github1s')
+    },
     search() {
       if (!this.searchValue) {
         return
@@ -21,7 +32,8 @@ const ReviewApp = {
       this.searchText = '查询中...'
       const xhr = new XMLHttpRequest()
       xhr.responseType = 'json'
-      xhr.open("GET", `https://vercel-serverless-five.vercel.app/api/index?q=${this.searchValue}`, true)
+      // xhr.open("GET", `https://www.npmjs.com/search/suggestions?q=${this.searchValue}`, true)
+      xhr.open("GET", `http://localhost:3000/api?q=${this.searchValue}`, true)
       xhr.send()
       xhr.onreadystatechange = () => {
         this.searchText = '查询'
